b2373a14198c827210326332cc272d560d3d6647
Kevin Ahn and Ryan Putman <kevin@>
2018-05-29 10:56:51 -0700

    Add sea lion section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-
 $(document).ready(function() {
   $(".clickable").click(function() {
     $(".initially-showing").toggle();
     $(".initially-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".initially-showing2").toggle();
+    $(".initially-hidden2").toggle();
   });
 });
