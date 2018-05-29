a906f468fba563f2f7df1083d15fa9659f2c8358
Kevin Ahn and Ryan Putman <kevin@>
2018-05-29 11:32:59 -0700

    add colors.html and links between pages

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,27 @@ $(document).ready(function() {
     $(".initially-showing").toggle();
     $(".initially-hidden").toggle();
   });
-});
-$(document).ready(function() {
   $(".clickable2").click(function() {
     $(".initially-showing2").toggle();
     $(".initially-hidden2").toggle();
   });
-});
-$(document).ready(function() {
   $(".clickable3").click(function() {
     $(".initially-hidden3").slideToggle();
+  });
+
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
